39a85e3fb19dc45e3ba2edfab5460ef60220a91b
realkcn <realkcn@gmail.com>
2017-05-02 01:45:55 +0800

    empty project complete

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 
 // Modules
 var webpack = require('webpack');
-// var autoprefixer = require('autoprefixer');
+var autoprefixer = require('autoprefixer');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -26,13 +26,120 @@ module.exports=function makeWebpackConfig() {
      */
     var config = {};
 
-    config.entry = path.resolve(__dirname, './src/js/main.js');
+    config.entry = {
+        yyssearch: path.resolve(__dirname, './src/js/main.js')
+    };
 
     config.output = {
         path: path.resolve(__dirname, './build'),
-        publicPath: isProd ? "dist" : "build/",
+        publicPath: (isProd || isDev) ? "/" : "build/",
         filename:'[name].bundle.js'
+    };
 
-        }
-        return config;
+    // Initialize module
+    config.module = {
+        rules: [{
+            // JS LOADER
+            // Reference: https://github.com/babel/babel-loader
+            // Transpile .js files using babel-loader
+            // Compiles ES6 and ES7 into ES5 code
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+        }, {
+            // CSS LOADER
+            // Reference: https://github.com/webpack/css-loader
+            // Allow loading css through js
+            //
+            // Reference: https://github.com/postcss/postcss-loader
+            // Postprocess your css with PostCSS plugins
+            test: /\.css$/,
+            // Reference: https://github.com/webpack/extract-text-webpack-plugin
+            // Extract css files in production builds
+            //
+            // Reference: https://github.com/webpack/style-loader
+            // Use style-loader in development.
+
+            use: isTest ? 'null-loader' : ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    {loader: 'css-loader', query: {sourceMap: true}},
+                    {loader: 'postcss-loader'}
+                ]
+            })
+        }, {
+            // ASSET LOADER
+            // Reference: https://github.com/webpack/file-loader
+            // Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
+            // Rename the file using the asset hash
+            // Pass along the updated reference to your code
+            // You can add here any file extension you want to get copied to your output
+            test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+            use: 'file-loader'
+        }, {
+            // HTML LOADER
+            // Reference: https://github.com/webpack/raw-loader
+            // Allow loading html through js
+            test: /\.html$/,
+            use: 'raw-loader'
+        }]
+    };
+
+    /**
+     * Plugins
+     * Reference: http://webpack.github.io/docs/configuration.html#plugins
+     * List: http://webpack.github.io/docs/list-of-plugins.html
+     */
+    config.plugins = [
+        new webpack.LoaderOptionsPlugin({
+            test: /\.scss$/i,
+            options: {
+                postcss: {
+                    plugins: [autoprefixer({browsers:['last 2 versions']})]
+                }
+            }
+        })
+    ];
+
+    // Skip rendering index.html in test mode
+    if (!isTest) {
+        // Reference: https://github.com/ampedandwired/html-webpack-plugin
+        // Render index.html
+        config.plugins.push(
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                inject: 'body'
+            }),
+
+            // Reference: https://github.com/webpack/extract-text-webpack-plugin
+            // Extract css files
+            // Disabled when in test mode or not in build mode
+            new ExtractTextPlugin({filename: 'css/[name].css', disable: !isProd, allChunks: true})
+        )
+    }
+
+    // Add build specific plugins
+    if (isDev || isProd) {
+        config.plugins.push(
+            // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+            // Only emit files when there are no errors
+            new webpack.NoEmitOnErrorsPlugin(),
+
+            // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+            // Minify all javascript, switch loaders to minimizing mode
+            new webpack.optimize.UglifyJsPlugin(),
+
+            // Copy assets from the public folder
+            // Reference: https://github.com/kevlened/copy-webpack-plugin
+            new CopyWebpackPlugin([{
+                from: __dirname + '/src/public'
+            }])
+        )
+    }
+
+    config.devServer = {
+        contentBase: './build',
+        stats: 'minimal'
+    };
+    return config;
 }();
